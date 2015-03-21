1b6697b7d9161207b2d3a4faa7d5623eba1319bf
unknown <etemnogrudova@gmail.com>
2015-03-21 13:43:07 +0200

    First commit

---
+++ app/src/main/java/com/example/kate/ble2/SampleGattAttributes.java
@@ -0,0 +1,25 @@
+package com.example.kate.ble2;
+
+import java.util.HashMap;
+
+/**
+ * Created by 123 on 19.03.2015.
+ */
+public class SampleGattAttributes {
+
+    private static HashMap<String, String> attributes = new HashMap();
+    public static String HEART_RATE_MEASUREMENT = "00002a37-0000-1000-8000-00805f9b34fb";
+    public static String CLIENT_CHARACTERISTIC_CONFIG = "00002902-0000-1000-8000-00805f9b34fb";
+    static {
+        // Sample Services.
+        attributes.put("0000180d-0000-1000-8000-00805f9b34fb", "Heart Rate Service");
+        attributes.put("0000180a-0000-1000-8000-00805f9b34fb", "Device Information Service");
+        // Sample Characteristics.
+        attributes.put(HEART_RATE_MEASUREMENT, "Heart Rate Measurement");
+        attributes.put("00002a29-0000-1000-8000-00805f9b34fb", "Manufacturer Name String");
+    }
+    public static String lookup(String uuid, String defaultName) {
+        String name = attributes.get(uuid);
+        return name == null ? defaultName : name;
+    }
+}
